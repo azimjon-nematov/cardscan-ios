18087fea3c0c2c1db3e36cba79676667b60f8786
azimjon_m2pro <99798110+azimjon-nematov@users.noreply.github.com>
2025-04-29 10:49:14 +0500

    fix

--- CardScan/CardScan/Classes/Expiry.swift
+++ CardScan/CardScan/Classes/Expiry.swift
@@ -4,9 +4,4 @@ public struct Expiry {
     public let string: String
     public let month: UInt
     public let year: UInt
-    
-    func display() -> String {
-        let twoDigitYear = self.year % 100
-        return String(format: "%02d/%02d", self.month, twoDigitYear)
-    }
 }
