e04fb5907f27f6f42d67ac36a1299439e493e880
azimjon_m2pro <99798110+azimjon-nematov@users.noreply.github.com>
2025-05-26 16:36:11 +0500

    delete PredictionResult and some functions

--- CardScan/CardScan/Classes/CreditCardOcr/CreditCardOcrPrediction.swift
+++ CardScan/CardScan/Classes/CreditCardOcr/CreditCardOcrPrediction.swift
@@ -43,34 +43,6 @@ public struct CreditCardOcrPrediction {
         return "\(month)/\(year)"
     }
     
-    public var expiryAsUInt: (UInt, UInt)? {
-        guard let month = expiryMonth.flatMap({ UInt($0) }) else { return nil }
-        guard let year = expiryYear.flatMap({ UInt($0) }) else { return nil }
-        
-        return (month, year)
-    }
-    
-    public var numberBox: CGRect? {
-        let xmin = numberBoxes?.map { $0.minX }.min() ?? 0.0
-        let xmax = numberBoxes?.map { $0.maxX }.max() ?? 0.0
-        let ymin = numberBoxes?.map { $0.minY }.min() ?? 0.0
-        let ymax = numberBoxes?.map { $0.maxY }.max() ?? 0.0
-        return CGRect(x: xmin, y: ymin, width: (xmax - xmin), height: (ymax - ymin))
-    }
-    
-    public var expiryBox: CGRect? {
-        return expiryBoxes.flatMap { $0.first }
-    }
-    
-    public var numberBoxesInFullImageFrame: [CGRect]? {
-        guard let boxes = numberBoxes else { return nil }
-        let cropOrigin = ocrCroppingRectangle.origin
-        return boxes.map { CGRect(x: $0.origin.x + cropOrigin.x,
-                                  y: $0.origin.y + cropOrigin.y,
-                                  width: $0.size.width,
-                                  height: $0.size.height) }
-    }
-    
     static func likelyExpiry(_ string: String) -> (String, String)? {
         guard let regex = try? NSRegularExpression(pattern: "^.*(0[1-9]|1[0-2])[./]([1-2][0-9])$") else {
             return nil
